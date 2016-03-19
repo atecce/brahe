a6db0309ed1757ad90e410db77f99df213a28c38
Alessandro Tecce <alte5103@colorado.edu>
2016-03-18 21:00:36 -0600

    Going to make songs objects that parse text from files. Too many problems inside the .py files.

---
+++ song.py
@@ -0,0 +1,18 @@
+class song:
+
+	artist = str()
+	album  = str()
+	track  = int()
+	title  = str()
+	lyrics = str()
+
+	def __init__(self, artist, album, track, title, filename):
+
+		self.artist = artist
+		self.album  = album
+		self.track  = track
+		self.title  = title
+
+		with open(filename) as f:
+
+			lyrics = f.read()
