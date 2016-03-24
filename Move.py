f512bdb6bb1beae84562dfae6bd8012445389073
Alessandro Tecce <alte5103@colorado.edu>
2016-03-23 21:48:52 -0600

    If you are what you say you are. Then have no fear.

--- Move.py
+++ Move.py
@@ -3,8 +3,9 @@ from inspiration.song import song
 class Move:
 
 	songs = [
-			song("The Foreign Exchange", "Connected", 	 3, "Raw Life", 	 "inspiration/raw_life.txt"),
-			song("Broken Social Scene",  "To Be You and Me", 6, "Major Label Debut", "inspiration/major_label_debut.txt")
+			song("The Foreign Exchange", "Connected", 	       3, "Raw Life", 	       "inspiration/raw_life.txt"),
+			song("Broken Social Scene",  "To Be You and Me",       6, "Major Label Debut", "inspiration/major_label_debut.txt"),
+			song("Lupe Fiasco",  	     "Lupe Fiasco's The Cool", 5, "Superstar", 	       "inspiration/superstar.txt")
 		]
 
 for song in Move.songs: print song.lyrics
