30ac6f934321eda0ae116e9b248ad1922f283d22
Alessandro Tecce <alte5103@colorado.edu>
2016-03-23 21:44:47 -0600

    Translation: It means I love you.

--- Move.py
+++ Move.py
@@ -3,7 +3,8 @@ from inspiration.song import song
 class Move:
 
 	songs = [
-			song("The Foreign Exchange", "Connected", 3, "Raw Life", "inspiration/raw_life.txt")
+			song("The Foreign Exchange", "Connected", 	 3, "Raw Life", 	 "inspiration/raw_life.txt"),
+			song("Broken Social Scene",  "To Be You and Me", 6, "Major Label Debut", "inspiration/major_label_debut.txt")
 		]
 
 for song in Move.songs: print song.lyrics
