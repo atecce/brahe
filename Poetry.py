a8b854ba36676dc563b13a3eb8d2968aed786790
Alessandro Tecce <alte5103@colorado.edu>
2016-04-04 17:44:59 -0600

    Focus.

--- Poetry.py
+++
@@ -1,9 +0,0 @@
-from inspiration.song import song
-
-class Poetry:
-
-	songs = [
-			song("Black Star", "Mos Def & Talib Kweli Are Black Star", 12, "Thieves in the Night", "inspiration/thieves_in_the_night.txt")
-		]
-
-for song in Poetry.songs: print song.lyrics
