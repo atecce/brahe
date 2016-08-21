68f6ae88821ef7988621e0c96a42053fd170c97c
atecce <atecce@icloud.com>
2016-08-21 19:37:48 -0400

    Begin investigating audio

--- main.go
+++ main.go
@@ -1,16 +1,53 @@
 package main
 
 import (
-	"flag"
-	"investigations/websites"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
 )
+
+//const trackID = 5151298
 
 func main() {
 
-	// set start flag
-	start := flag.String("s", "0", "Specify start artist of crawl.")
-	flag.Parse()
+	// start counter at 0
+	var trackID int
+	for {
 
-	// start the investigation
-	websites.Investigate(*start)
+		// get json
+		if resp, err := http.Get("http://api.soundcloud.com/tracks/" + strconv.Itoa(trackID) +
+			"?client_id=" + os.Getenv("CLIENT_ID")); err != nil {
+			panic(err)
+		} else {
+			defer resp.Body.Close()
+
+			// increment ID
+			fmt.Println(trackID)
+			trackID++
+
+			// set decoder
+			dec := json.NewDecoder(resp.Body)
+
+			// read until break
+			for {
+
+				// initalize track info
+				var track map[string]interface{}
+
+				// break on err (hopefully EOF)
+				if err := dec.Decode(&track); err != nil {
+					log.Println(err)
+					break
+				}
+
+				// iterate through keys and print values
+				for k := range track {
+					fmt.Println(k, track[k])
+				}
+			}
+		}
+	}
 }
