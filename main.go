156d65ee8544fd551d22c6c914d1ae85c0cb8c92
atecce <atecce@icloud.com>
2016-10-15 16:52:33 -0400

    Add semaphore to deal with network bottleneck

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/url"
 	"os"
 	"strconv"
+	"sync"
 
 	"github.com/atecce/brahe/heavens"
 )
@@ -16,7 +17,21 @@ const (
 	family = "favorites"
 )
 
+var wg sync.WaitGroup
+
 func main() {
+
+	// check user input
+	if len(os.Args) != 2 {
+		println("Usage: brahe <braching factor>")
+		os.Exit(1)
+	}
+
+	// branching factor
+	b, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
 
 	// initalize file
 	favorites, err := os.Create("favorites.txt")
@@ -28,37 +43,46 @@ func main() {
 	// observe the heavens
 	for id := 1; ; id++ {
 
-		// construct method
-		methodPath := table + "/" + strconv.Itoa(id)
-		method := &url.URL{
-			Scheme:   "http",
-			Host:     "api.soundcloud.com",
-			Path:     methodPath,
-			RawQuery: "client_id=" + os.Getenv("CLIENT_ID"),
+		// look at b stars at a time
+		if id%b == 0 {
+			wg.Wait()
 		}
+		wg.Add(1)
+		go func(id int) {
+			defer wg.Done()
 
-		// get user info
-		body := heavens.Observe(method)
-		if body == nil {
-			continue
-		}
-		var user map[string]interface{}
-		json.Unmarshal(body, &user)
-		userID := strconv.FormatFloat(user["id"].(float64), 'f', -1, 64)
-
-		// get favoriters info
-		method.Path = methodPath + "/" + family
-		body = heavens.Observe(method)
-		var songs []interface{}
-		json.Unmarshal(body, &songs)
-		for _, song := range songs {
-			trackID := strconv.FormatFloat(song.(map[string]interface{})["id"].(float64), 'f', -1, 64)
-
-			// record the observation
-			_, err := favorites.WriteString(userID + "\t" + trackID + "\n")
-			if err != nil {
-				panic(err)
+			// construct method
+			methodPath := table + "/" + strconv.Itoa(id)
+			method := &url.URL{
+				Scheme:   "http",
+				Host:     "api.soundcloud.com",
+				Path:     methodPath,
+				RawQuery: "client_id=" + os.Getenv("CLIENT_ID"),
 			}
-		}
+
+			// get user info
+			body := heavens.Observe(method)
+			if body == nil {
+				return
+			}
+			var user map[string]interface{}
+			json.Unmarshal(body, &user)
+			userID := strconv.FormatFloat(user["id"].(float64), 'f', -1, 64)
+
+			// get favoriters info
+			method.Path = methodPath + "/" + family
+			body = heavens.Observe(method)
+			var songs []interface{}
+			json.Unmarshal(body, &songs)
+			for _, song := range songs {
+				trackID := strconv.FormatFloat(song.(map[string]interface{})["id"].(float64), 'f', -1, 64)
+
+				// record the observation
+				_, err := favorites.WriteString(userID + "\t" + trackID + "\n")
+				if err != nil {
+					panic(err)
+				}
+			}
+		}(id)
 	}
 }
